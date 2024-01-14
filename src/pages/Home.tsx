f022846e3021898fa0914bd131e242f85d776776
uiseop <bono521@naver.com>
2024-01-14 19:53:20 +0900

    ADD: 홈 페이지에 버튼, 로고 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,23 @@
+import styled from '@emotion/styled';
+import { ReactComponent as LogoBig } from '@assets/icons/LogoBig.svg';
+import colors from '@assets/colors.json';
+import { Button } from '@components/common';
+
 export const Home = () => {
-	return <>Hello this is home</>;
+	return (
+		<Wrapper>
+			<LogoBig />
+			<Button content="주문하러 가기" borderRadius={20} />
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.main({
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'center',
+	alignItems: 'center',
+	height: '100vh',
+	gap: 41,
+	backgroundColor: colors.black,
+});
