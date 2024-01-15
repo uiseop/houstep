2c1f98d3714ca7f86e300d63aa8fe5b9527a9da1
uiseop <bono521@naver.com>
2024-01-15 18:32:29 +0900

    EDIT: 헤더 고정

--- src/components/order/Header.tsx
+++ src/components/order/Header.tsx
@@ -17,8 +17,13 @@ export const Header = () => {
 const Wrapper = styled.header({
 	height: 57,
 	backgroundColor: colors.black,
+	position: 'fixed',
+	left: 0,
+	right: 0,
+	top: 0,
 	display: 'flex',
 	alignItems: 'center',
 	padding: '0 12px',
 	boxShadow: `0px 4px 4px rgba(0, 0, 0, 0.25)`,
+	zIndex: 1,
 });
--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -100,5 +100,5 @@ const CardContainer = styled.ul`
 	display: flex;
 	flex-direction: column;
 	gap: 18px;
-	margin: 18px 24px 188px 25px;
+	margin: 75px 24px 188px 25px;
 `;
